7997b9f475f568da5f23d97fbd61c3913090a5ef
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-12-03 01:19:58 +0000

    first commit

---
+++ lib/movie/openings.rb
@@ -0,0 +1,8 @@
+require "movie/openings/version"
+
+module Movie
+  module Openings
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/movie/openings/version.rb
@@ -0,0 +1,5 @@
+module Movie
+  module Openings
+    VERSION = "0.1.0"
+  end
+end
